254c05d90a2814fa057e570fef5338865e019718
Gbengdan <gbengdan@gmail.com>
2024-09-03 18:18:59 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,185 @@
+// let y2 = 10
+// console.log(y2)
+
+// // let  username ="my  name is gbenga"
+// console.log(username)
+// let username = "my---- name --- is --- gbenga"
+// console.log(username)
+
+// let $10000forgbenga ="in my account"
+// console.log($10000forgbenga)
+// let _penthouse ="belongs to me"
+
+// console.log(_penthouse)
+// let color = "red"
+// console.log(color)
+// let Color = "green"
+// console.log(Color)
+// let mycolors ="red,blue,voilet,purple,indigo,white,orange,buguary"
+// console.log(mycolors)
+// let mySchoolname ="fadu memorial primary school"
+// console.log(mySchoolname)
+// let Myschoolname = "Global excellence comprehensive high school ipaja ayobo"
+// console.log(Myschoolname)
+// let mySchoolName ="Ladoke akintola university of technology ogbomoso,oyo state nigeria"
+// console.log(mySchoolName)
+// let MyschoolName ="Samtech IT world quality assurance tester and we are the gate keeper for all developers"
+// console.log(MyschoolName)
+// var subject = "Geography"
+// subject = "Biology"
+// console.log(subject)
+// const subject ="Geography"
+// subject ="Biology"
+// console.log(subject)
+
+// // const myAge ="37"
+// // console.log(myAge)
+// // const myAge ="37"
+// // console.log(tpypeof myAge )
+
+// // const myAge ="thirty" 
+// // console.log(typeof(myAge))
+// const myAge = 30
+// console.log(myAge +50)
+
+// const myAge = 30
+// const myAge = "thirty"
+// console.log(typeof(myAge))
+// const myAge ="i will be be thirty year old 2days time"
+// console.log(typeof(myAge))
+// const myAge = 30
+// console.log(myAge +20)
+
+// const myAge = 30
+// console.log(myAge + 20) 
+// const myAge = 30
+// console.log(myAge * 2)
+// const myAge = 30
+// console.log(typeof(myAge))
+// const myAge = "Thirty"
+// console.log(typeof(myAge))
+// const age = 30
+// console.log(typeof(age)) 
+// const myAge = "Thirty year old in 2d days time"
+// console.log(typeof(myAge))
+// const myAge = 30
+// console.log(myAge + 20)  
+// const myAge = 30
+
+// console.log(myAge * 2)
+// const multiplyAge = myAge
+// console.log(multiplyAge * 345) 
+// const myAge = 30
+// const  nextAge = 2
+// console.log(myAge * nextAge)
+// const myfavlanguage ="javascript"
+// console.log(typeof(myfavlanguage))
+// console.log( "my favorite language is " +  myfavlanguage)
+// const mybestfriend = "Ayokusibe"
+// console.log(typeof(mybestfriend))
+// console.log("the name of my best friend is" + mybestfriend)
+// const Age = 20 
+// const name = "john"
+// console.log("my name is" + name) 
+// const Age = 20
+// const name = "john"
+// const sentence ="my name is " + name
+// console.log(sentence)
+// const Age = 20
+// const name ="john"
+// const sentence = "my name is"  +  name  + "."   + "i  am"   +  Age  +  "years old."
+// console.log(sentence)
+// const Age = 20
+// const name = "Magodo"
+// const Duplex = "white pent house"
+// const sentence = "i have total number of"   +   Age  +  "."  + Duplex  +  " gaints building"  +      "in"      +  name
+// console.log(sentence)
+
+// const isBritish = true
+// const isNight = false
+// const Userdetails = null
+// console.log(typeof(Userdetails)) 
+
+// const addition = 59 + 2
+// console.log(addition)
+// const subtraction = 204567 - 188886
+// console.log(subtraction)
+// const division = 50000 / 500
+// console.log(divison)
+// const addition = 67 + (-9)
+// console.log(addition)
+// const addition = 73 + (-8)
+// console.log(addition )
+// const multiplication = 84*3
+// console.log(multiplication)
+// const division = 100/ 5
+// console.log(division)
+// const modulus = 100%4
+// console.log(modulus )
+// const modulus = 100%3
+// console.log(modulus)
+
+//  const compare = 4 > 3
+//  console.log(compare)
+//  const compare = 65 < 24
+//  console.log(compare)
+// const compare = 2 < -5
+// console.log(compare)
+// const compare = 1 > -3
+// console.log(compare)
+// console.log(4==4)
+// const compare = 4==="-4"
+// console.log(compare)
+// console.log("4"==="4")
+// console.log('6'==='4')  
+// const compare =(4 + '4')
+// console.log(compare)
+// const compare = 4 + '4'
+// console.log(typeof(4+'4'))
+// console.log(typeof(4+4))
+// const compare = 4 + 'eggs'
+// console.log(compare) 
+// const Age = 18
+// console.log(18<=Age)
+// const Age = 18
+// console.log(18>=Age)
+// const Age = 18 
+// console.log(20>=Age)
+// const Age =25
+// const age = 45
+// console.log(age>=Age)
+
+// const myAge = 57
+// console.log(typeof(myAge))
+// const myAge = 57
+// console.log(myAge)
+// const myAge ='Thirty'
+// console.log(typeof(myAge))
+// const age =67
+// console.log(typeof(age))
+// const Myage =53
+// console.log(typeof(Myage))
+// const myAge ="53"
+// console.log(typeof(myAge))
+// console.log(myAge)
+// const day = "monday"
+// const position = 1
+// const output = "Today is " + day + "." + "it is the " +  position + "st day of the week"
+// console.log(output)
+const day = "Friday"
+const position = 5
+const output = "Today is"   +  day  + "." + "it is the"   +    position   +   "th day of the week"
+console.log(output)
+const Position = 1
+const Day = "Monday"
+// const Output = " 1st day of the week"  + Position + "." +  "is"  +    Day
+const Output = Position + "st day of the week"   +  "is"     +  Day 
+console.log(Output)
+
+const outpuT = "Today is thursday"
+const daY = 4
+const lead = outpuT +  "It is the"   +   daY   +   "th of the week" + "."
+console.log(lead) 
+
+
+
